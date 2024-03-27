5eb301cda89a8996b85ba9972d31ae13dea63d82
Nishan Shrestha <158047054+nishanshrestha04@users.noreply.github.com>
2024-03-27 15:07:22 +0000

    new commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "nishanshreatha@gmailcom"
+var accountPassword = "12345"
+accountCity = 'Balkot'
+let accountState;
+// const make the value constant  that cannot be changed 
+
+// accountId = 2
+accountEmail = 'nin@gmai.com'
+accountPassword = '2121'
+accountCity = 'Bhaktapur' 
+
+// console.log(accountId);
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail, accountPassword, accountCity, accountState])

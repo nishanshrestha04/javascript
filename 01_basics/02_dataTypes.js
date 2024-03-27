6bc15a7947b4a74a38f1c6a446b609666bc58943
Nishan Shrestha <158047054+nishanshrestha04@users.noreply.github.com>
2024-03-27 15:29:15 +0000

    second file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use sctrict"; // treat all JS code as newer version
+
+// alert("Hello") // its the browser version
+
+ // code readability should be high
+
+ let name = "nishan" // string dataType
+ let age = 18 // integer dataType
+ let isLogggedIn = false // boolean dataType
+
+// primitive dataType
+
+ // number => 2 to power 53
+ // bigint
+ // string = > ""
+ //boolean => true/false
+ // null => stand alone value
+ // undefined
+ // symbol => unique 
+
+
+// object
+console.log(typeof "nishan")
+console.log(typeof undefined) // undefined
+console.log(typeof null); // objext

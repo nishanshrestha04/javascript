0a9c53b753fd2f14caf421e79b5687bbdef99958
Nishan Shrestha <158047054+nishanshrestha04@users.noreply.github.com>
2024-03-29 16:42:45 +0000

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,4 +21,40 @@ let booleanIsLoggedIn =  Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber) // shows string
+// console.log(typeof stringNumber) // shows string
+
+// ******************** Operations ***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+
+let str1 = "Hello"
+let str2 = " Nishan"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + "2") // 12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(true) // true
+// console.log(+true) // 1
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)

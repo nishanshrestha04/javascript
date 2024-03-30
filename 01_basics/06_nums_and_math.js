329532bc948575d86ba26c4ee4170b7a56bcd7d6
Nishan Shrestha <158047054+nishanshrestha04@users.noreply.github.com>
2024-03-30 06:28:01 +0000

    maths and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+// let score = 400
+// console.log(typeof score);
+
+// let balance = new Number(400)
+
+// console.log(typeof balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)) // create a float value
+
+
+// let otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3)) // gives the precise value
+
+
+// let hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-45)); // gives the absolute value
+// console.log(Math.round(4.3)); // gives the rounded ofthe valuee value
+
+console.log((Math.random() * 10 ) + 1);
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
